8be46efac75ec9c43dc53e6d20fc75286b413f30
isylynch <isylynch@hotmail.com>
2018-12-01 17:58:17 +0000

    Add files via upload
    finished views, need to connect to server, added buttons, changed background

--- script.js
+++ script.js
@@ -10,28 +10,18 @@ var tileDict = ["Tiles/BarbedWire.png", "Tiles/Dirt.png", "Tiles/Stone.png", "Ti
 var tileWidth = 40;
 var tileHeight = 40;
 
-var btnStartX = 820;
-var btnStartY = 450;
-var btnWidth = 154;
-var btnHeight = 92;
+var btnStartX = 400;
+var btnStartY = 300;
+var btnWidth = 150;
+var btnHeight = 49;
 
-//TODO: add in values
-var btn2StartX = 0;
-var btn2StartY = 0;
-var btn2Width = 0;
-var btnHeight = 0;
+var btn2StartX = 400;
+var btn2StartY = 380;
+var btn2Width = 150;
+var btn2Height = 49;
 
 var currentScreen = "start";
 
-function startMenu() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    currentScreen = "start";
-    //TODO: change text & font options & starting position
-    ctx.font = "52px impact";
-    ctx.fillText("Game", 400, 200);
-    ctx.fillText("Join Game", 400, 300);
-    ctx.fillText("Create Game", 400, 350);
-}
 
 function createImage(file, x, y) {
     var img = new Image();
@@ -59,20 +49,65 @@ function drawButton(file, x, y, w, h) {
     var btn = new Image();
     btn.src = file;
     btn.onload = function() {
-        ctx.drawImage(btn, x, y, btn.width, btn.height);
+        ctx.drawImage(btn, x, y, w, h);
     };
 }
 
-function gameScreen() {
+
+function onClick(e) {
+    if (currentScreen == "game" && e.clientX >= btnStartX && e.clientX <= btnStartX + btnWidth && e.clientY >= btnStartY && e.clientY <= btnStartY + btnHeight) {
+        clicked();
+    } else if (currentScreen == "end" && e.clientX >= btnStartX && e.clientX <= btnStartX + btnWidth && e.clientY >= btnStartY && e.clientY <= btnStartY + btnHeight) {
+        startMenu();
+    } else if (currentScreen == "start" && e.clientX >= btnStartX && e.clientX <= btnStartX + btnWidth && e.clientY >= btnStartY && e.clientY <= btnStartY + btnHeight) {
+        var input = prompt("Please enter the game code:");
+    } else if (currentScreen == "start" && e.clientX >= btn2StartX && e.clientX <= btn2StartX + btn2Width && e.clientY >= btn2StartY && e.clientY <= btn2StartY + btn2Height) {
+        waitingScreen();
+    }
+}
+
+
+
+//VIEWS
+function startMenu() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    currentScreen = "start";
+    var btnStartX = 400;
+    var btnStartY = 300;
+    var btnWidth = 150;
+    var btnHeight = 49;
+
+    var btn2StartX = 400;
+    var btn2StartY = 380;
+    var btn2Width = 150;
+    var btn2Height = 49;
+
+    ctx.font = "52px impact";
+    ctx.fillText("Game", 400, 200);
+    drawButton("Buttons/JoinGame.png", btnStartX, btnStartY, btnWidth, btnHeight);
+    drawButton("Buttons/CreateGame.png", btn2StartX, btn2StartY, btn2Width, btn2Height);
+}
+
+function waitingScreen() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    currentScreen = "wait";
+    
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.font = "52px impact";
+    ctx.fillText("Waiting ...", 400, 200);
+    ctx.fillText("code here", 400, 300);
+}
+
+function gameScreen(map2D, playerY, points) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    canvas.style.cssText = "background: #C7D7F0";
     currentScreen = "game";
     btnStartX = 820;
     btnStartY = 450;
     btnWidth = 154;
     btnHeight = 92;
     //TODO: specify background tiles from server
-    var background = [[0,0,1,0,1,0,0,2,0,0,0,3,0,1,1,2,0,0,3,0,1,2,0,0,0],[0,3,0,2,1,3,2,2,0,1,0,0,0,0,0,1,0,0,2,2,0,0,0,0,0], [0,0,1,0,1,0,0,2,0,0,0,3,0,1,1,2,0,0,3,0,0,0,2,3,3]];
-    drawBackground(background);
+    drawBackground(map2D);
     //TODO: change button depending on server
     if (left) {
         drawButton("Buttons/LeftButton.png", btnStartX, btnStartY, btnWidth, btnHeight);
@@ -83,45 +118,25 @@ function gameScreen() {
     } else {
         drawButton("Buttons/button.png", btnStartX, btnStartY, btnWidth, btnHeight);
     }
-    
-}
-
-function onClick(e) {
-    if (currentScreen == "game" && e.clientX >= btnStartX && e.clientX <= btnStartX + btnWidth && e.clientY >= btnStartY && e.clientY <= btnStartY + btnHeight) {
-        //TODO: send button click info to server
-        console.log("action");
-    } else if (currentScreen == "end" && e.clientX >= btnStartX && e.clientX <= btnStartX + btnWidth && e.clientY >= btnStartY && e.clientY <= btnStartY + btnHeight) {
-        startMenu();
-    } else if (currentScreen == "start" && e.clientX >= btnStartX && e.clientX <= btnStartX + btnWidth && e.clientY >= btnStartY && e.clientY <= btnStartY + btnHeight) {
-        var input = prompt("Please enter the game code:");
-    } else if (currentScreen == "start" && e.clientX >= btn2StartX && e.clientX <= btn2StartX + btn2Width && e.clientY >= btn2StartY && e.clientY <= btn2StartY + btn2Height) {
-        waitingScreen();
-    }
 }
 
 function gameOverScreen() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     currentScreen = "end";
     //TODO: change btn values
-    btnStartX = 500;
-    btnStartY = 400;
-    btnWidth = 154;
-    btnHeight = 92;
+    var btnStartX = 400;
+    var btnStartY = 300;
+    var btnWidth = 150;
+    var btnHeight = 49;
     ctx.font = "52px impact";
     ctx.fillText("Game Over", 400, 200);
-    drawButton("button.png", btnStartX, btnStartY, btnWidth, btnHeight);
+    drawButton("Buttons/NewGame.png", btnStartX, btnStartY, btnWidth, btnHeight);
 
 }
 
-function waitingScreen() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    currentScreen = "wait";
-    //TODO: change btn values
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.font = "52px impact";
-    ctx.fillText("Waiting ...", 400, 200);
-}
 
 startMenu();
 //gameOverScreen();
-//gameScreen();
+//var background = [[0,0,1,0,1,0,0,2,0,0,0,3,0,1,1,2,0,0,3,0,1,2,0,0,0],[0,3,0,2,1,3,2,2,0,1,0,0,0,0,0,1,0,0,2,2,0,0,0,0,0], [0,0,1,0,1,0,0,2,0,0,0,3,0,1,1,2,0,0,3,0,0,0,2,3,3]];
+//gameScreen(background, 0, 0);
+//gameOverScreen();
